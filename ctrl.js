845917ffebc544b07e317d0ad71e7d6172e77bd6
Firdaws Idris <firzasi.fi@gmail.com>
2023-10-23 11:05:59 +0100

    first commit

---
+++ ctrl.js
@@ -0,0 +1,8 @@
+let signIn = document.getElementById("signIn");
+let singUp = document.getElementById("signUp");
+let in = document.getElementsByClassName("in");
+let up = document.getElementsByClassName("up");
+
+onclick signIn function{
+    up = display.none;
+}
